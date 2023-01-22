d07798410a7d8e772bdb85c879c18a2898cac67e
miguel9177 <luis2.128.b80@gmail.com>
2023-01-22 03:38:10 +0000

    commented code, next step refactor it

--- Week1ClienyUnity/Assets/Scripts/NetworkManager.cs
+++ Week1ClienyUnity/Assets/Scripts/NetworkManager.cs
@@ -11,48 +11,66 @@ public class NetworkManager : MonoBehaviour
 {
     public struct UdpState
     {
-        public UdpClient u;
-        public IPEndPoint e;
+        public UdpClient _udpClient;
+        public IPEndPoint _ipEndPoint;
     }
 
+    //this stores the ping timer, so that i can know my ping
     System.Diagnostics.Stopwatch pingTimer = new System.Diagnostics.Stopwatch();
-    static UdpClient client;
-    static IPEndPoint ep;
+    //this stores the state of the udp
     static UdpState state;
+    //this stores the Timer
     TimeSpan timer = new TimeSpan();
-    public TextMeshProUGUI txt;
+    //this stores the ui text that stores the ping
+    public TextMeshProUGUI txtPing;
 
+    //this stores the player name
+    static string playerName = "Miguel";
 
-    static string playerName = "Miniking";
-
-    //string ipAdress = "127.0.0.1";
-    //string ipAdress = "10.1.42.129";
     string ipAdress = "192.168.0.38";
+
     // Start is called before the first frame update
     void Start()
     {
+        //this creates a new client
         UdpClient client = new UdpClient();
-        IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAdress), 9050); // endpoint where server is listening (testing localy)
+        //this creates a new ipEndPoint using the ip adress and the port 9050
+        IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAdress), 9050); 
+        //we connect the client
         client.Connect(ep);
 
+        //variable with the first entrance message so that the server knows that this is the first connection made
         string myMessage = "FirstEntrance";
+        //this transforms the first entrance message into an array
         byte[] array = Encoding.ASCII.GetBytes(myMessage);
+        //we send the message to the server
         client.Send(array, array.Length);
 
+        //this starts the infinite loop void that will always be receiving the information from the server
         client.BeginReceive(ReceiveAsyncCallback, state);
 
+        //this will receive all messages from the server
         void ReceiveAsyncCallback(IAsyncResult result)
         {
-
-            byte[] receiveBytes = client.EndReceive(result, ref ep); //get the packet
-            string receiveString = Encoding.ASCII.GetString(receiveBytes); //decode the packet
-            Debug.Log("Received " + receiveString + " from " + ep.ToString()); //display the packet
+            //this gets the packet from the server in bytes
+            byte[] receiveBytes = client.EndReceive(result, ref ep); 
+            //this transforms it in a string 
+            string receiveString = Encoding.ASCII.GetString(receiveBytes); 
+            //this writes on the console its message
+            Debug.Log("Received " + receiveString + " from " + ep.ToString()); 
+            //this recalls this function to make it loop infinitely
             client.BeginReceive(ReceiveAsyncCallback, state); //self-callback, meaning this loops infinitely
+            //this stops the ping timer
             pingTimer.Stop();
+            //this gets the time elapsed
             timer = pingTimer.Elapsed;
+            //store the message to send to the server
             string myMessage2 = "PlayerName: " + playerName + " / Ping: " + timer.Milliseconds;
+            //make the message a byte array, since we always need to do this
             byte[] array2 = Encoding.ASCII.GetBytes(myMessage2);
+            //we send the information
             client.Send(array2, array2.Length);
+            //we restart the ping timer and start it again
             pingTimer.Restart();
             pingTimer.Start();
         }
@@ -62,7 +80,7 @@ public class NetworkManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        txt.text = "Ping: " + timer.Milliseconds;
+        txtPing.text = "Ping: " + timer.Milliseconds;
     }
 
 }
