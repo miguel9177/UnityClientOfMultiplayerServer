ae54ca922ae62d05d01d0e0e4454e24788902e34
miguel9177 <luis2.128.b80@gmail.com>
2023-01-22 05:24:00 +0000

    refractored code and improved

--- Week1ClienyUnity/Assets/Scripts/NetworkManager.cs
+++ Week1ClienyUnity/Assets/Scripts/NetworkManager.cs
@@ -15,15 +15,8 @@ public class NetworkManager : MonoBehaviour
         public IPEndPoint _ipEndPoint;
     }
 
-    //this stores the ping timer, so that i can know my ping
-    System.Diagnostics.Stopwatch pingTimer = new System.Diagnostics.Stopwatch();
     //this stores the state of the udp
-    static UdpState state;
-    //this stores the Timer
-    TimeSpan timer = new TimeSpan();
-    //this stores the ui text that stores the ping
-    public TextMeshProUGUI txtPing;
-
+    static UdpState state = new UdpState();
     //this stores the player name
     static string playerName = "Miguel";
 
@@ -32,57 +25,99 @@ public class NetworkManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //this creates a new client
-        UdpClient client = new UdpClient();
-        //this creates a new ipEndPoint using the ip adress and the port 9050
-        IPEndPoint ep = new IPEndPoint(IPAddress.Parse(ipAdress), 9050); 
-        //we connect the client
-        client.Connect(ep);
+        ConnectToServer();
 
-        //variable with the first entrance message so that the server knows that this is the first connection made
-        string myMessage = "FirstEntrance";
-        //this transforms the first entrance message into an array
-        byte[] array = Encoding.ASCII.GetBytes(myMessage);
-        //we send the message to the server
-        client.Send(array, array.Length);
+        //get the message that is necessary to send the server for a new connection
+        byte[] messageToSendForLogin = GetMessageToLoginToServer();
+        //start the loop of sending messages to the server
+        state._udpClient.BeginSend(messageToSendForLogin, messageToSendForLogin.Length, new AsyncCallback(SendMessageAssyncCallback), state);
 
         //this starts the infinite loop void that will always be receiving the information from the server
-        client.BeginReceive(ReceiveAsyncCallback, state);
-
+        state._udpClient.BeginReceive(ReceiveMessageAsyncCallback, state);
+        
         //this will receive all messages from the server
-        void ReceiveAsyncCallback(IAsyncResult result)
+        void ReceiveMessageAsyncCallback(IAsyncResult result)
         {
             //this gets the packet from the server in bytes
-            byte[] receiveBytes = client.EndReceive(result, ref ep); 
-            //this transforms it in a string 
-            string receiveString = Encoding.ASCII.GetString(receiveBytes); 
-            //this writes on the console its message
-            Debug.Log("Received " + receiveString + " from " + ep.ToString()); 
+            byte[] receiveBytes = state._udpClient.EndReceive(result, ref state._ipEndPoint);
+            //send the received message to the function that handles the received messages
+            ReceivedMessageFromServer(receiveBytes);
+
             //this recalls this function to make it loop infinitely
-            client.BeginReceive(ReceiveAsyncCallback, state); //self-callback, meaning this loops infinitely
-            //this stops the ping timer
-            pingTimer.Stop();
-            //this gets the time elapsed
-            timer = pingTimer.Elapsed;
-            //store the message to send to the server
-            string myMessage2 = "PlayerName: " + playerName + " / Ping: " + timer.Milliseconds;
-            //make the message a byte array, since we always need to do this
-            byte[] array2 = Encoding.ASCII.GetBytes(myMessage2);
-            //we send the information
-            client.Send(array2, array2.Length);
-            //we restart the ping timer and start it again
-            pingTimer.Restart();
-            pingTimer.Start();
+            state._udpClient.BeginReceive(ReceiveMessageAsyncCallback, state); //self-callback, meaning this loops infinitely
+        }
+
+        //this will send messages to the server
+        void SendMessageAssyncCallback(IAsyncResult result)
+        {
+            try
+            {
+                state._udpClient.EndSend(result);
+
+                byte[] messageToSend = GetMessageToSendToServer();
+                state._udpClient.BeginSend(messageToSend, messageToSend.Length, new AsyncCallback(SendMessageAssyncCallback), state);
+
+                Debug.Log("Message sent successfully!");
+            }
+            catch (SocketException ex)
+            {
+                Debug.Log("Message Error " + ex);
+            }
+        }
+
+    }
+
+    //this connects to the server
+    private void ConnectToServer()
+    {
+        //this creates a new client
+        state._udpClient = new UdpClient();
+        //this creates a new ipEndPoint using the ip adress and the port 9050
+        state._ipEndPoint = new IPEndPoint(IPAddress.Parse(ipAdress), 9050);
+        //we connect the client
+        state._udpClient.Connect(state._ipEndPoint);
+    }
+
+    //this will get the message that we want to send to the server
+    private Byte[] GetMessageToLoginToServer()
+    {
+        //send the first message
+        byte[] array = Encoding.ASCII.GetBytes("FirstEntrance");
+        return array;
+    }
+
+    //this will get the message that we want to send to the server
+    private Byte[] GetMessageToSendToServer()
+    {
+        //store the message to send to the server
+        string myMessage2 = "PlayerName: " + playerName + " / Ping: HAVENT MADE PING TIMER YET";
+        //make the message a byte array, since we always need to do this
+        byte[] array = Encoding.ASCII.GetBytes(myMessage2);
+        return array;
+    }
+
+    //this will read the message received from the server
+    private void ReceivedMessageFromServer(byte[] receiveBytes)
+    {
+        //this transforms it in a string 
+        string receiveString = Encoding.ASCII.GetString(receiveBytes);
+        //this writes on the console its message
+        Debug.Log("Received " + receiveString + " from " + state._ipEndPoint.ToString());
+    }
+
+    //this closes the socket when we leave the progran
+    private void CloseSocket()
+    {
+        if (state._udpClient != null)
+        {
+            state._udpClient.Close();
         }
     }
 
-
-    // Update is called once per frame
-    void Update()
+    private void OnApplicationQuit()
     {
-        txtPing.text = "Ping: " + timer.Milliseconds;
+        CloseSocket();
     }
-
 }
 
 
